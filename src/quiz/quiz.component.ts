9e04d099e6940a68453e3ad7f3fd77d80db6ecce
Daniel Muckelbauer <daniel.muckelbauer@sinc.de>
2024-09-01 16:50:55 +0200

    Stepper

--- src/quiz/quiz.component.ts
+++ src/quiz/quiz.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
+import { MatStep, MatStepper, MatStepperNext, MatStepperPrevious } from "@angular/material/stepper";
 
 @Component({
   selector: 'app-quiz',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatStep, MatStepper, MatStepperNext, MatStepperPrevious],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.scss'
 })
